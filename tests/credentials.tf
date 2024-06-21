c7a9cb9d357e9d2de030268574d3760d06fabc06
Marcin Szremski <msz@spyro-soft.com>
2024-06-21 14:27:31 +0200

    creds update

--- tests/credentials.tf
+++ tests/credentials.tf
@@ -9,25 +9,6 @@ provider "aws" {
     }
   }
 }
-/*provider "aws" {
-  region                   = var.region
-#  shared_config_files      = ["$HOME/.aws/credentials"]
-#  shared_credentials_files = ["$HOME/.aws/credentials"]
-#  profile                  = "ukri"
-  default_tags {
-    tags = {
-      ManagedByTerraform = "YES"
-    }
-  }
-}
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.53.0"
-    }
-  }
-}*/
 terraform {
   required_providers {
     aws = {
