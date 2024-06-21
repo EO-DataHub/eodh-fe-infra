870b1176d9dbdc0f82f065e43815edc727216aee
Marcin Szremski <msz@spyro-soft.com>
2024-06-21 14:06:53 +0200

    creds reconfiguration

--- tests/credentials.tf
+++ tests/credentials.tf
@@ -3,20 +3,34 @@ variable "region" {
 }
 provider "aws" {
   region                   = var.region
-  shared_config_files      = ["$HOME/.aws/credentials"]
-  shared_credentials_files = ["$HOME/.aws/credentials"]
-  profile                  = "ukri"
+#  shared_config_files      = ["$HOME/.aws/credentials"]
+#  shared_credentials_files = ["$HOME/.aws/credentials"]
+#  profile                  = "ukri"
   default_tags {
     tags = {
       ManagedByTerraform = "YES"
     }
   }
 }
+/*terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.53.0"
+    }
+  }
+}*/
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.53.0"
+    }
+  }
+  cloud {
+    organization = "EODH"
+    workspaces {
+      name = "TESTS-eodh-fe-infra"
     }
   }
 }
